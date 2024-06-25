490897646267f6734b920320f4b661ded22a137d
Nyaadu <nyaadu@gmail.com>
2024-06-25 15:21:38 -0700

    kk

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - Make alphabet
- * Return: void
+ * main - check the code
  *
+ * Return: Always 0.
  */
 
-void print_alphabet(void)
+int main(void)
+
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	_putchar('\n');
+    print_alphabet();
+    return (0);
 }
