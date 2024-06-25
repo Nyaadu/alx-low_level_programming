de16d13e12df2a9bcbbbe09dbd4406bf8162020a
Nyaadu <nyaadu@gmail.com>
2024-06-25 13:53:11 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _putchar - writes the chacter c
- * @c: Character to print
- * Return: On sucess 1.
- * On error, -1 is returned, and no is set appropriately
+ * main - Entry point
+ * Return: Always 0
  *
  */
 
-int _putchar(char c)
+int main(void)
 
 {
-	return (fwrite(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('/n');
 }
