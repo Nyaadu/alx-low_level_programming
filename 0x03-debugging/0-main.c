99624b653880facc9d829130cb85259137a96713
Nyaadu <nyaadu@gmail.com>
2024-06-27 09:41:24 -0700

    g

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - tests function that prints if integer is positive or negative
+/**main - tests function that prints if integer is positive or negative
  * Return: 0
+ *
  */
 
 int main(void)
@@ -13,5 +13,6 @@ int main(void)
 	
 	i = 0;
 	positive_or_negative(i);
+	
 	return (0);
 }
