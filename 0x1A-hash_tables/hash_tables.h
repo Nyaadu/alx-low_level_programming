67e4eb0c7687e4eef7ed52d17d910a706d847258
Nyaadu <nyaadu@gmail.com>
2024-10-11 09:27:57 +0000

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -79,4 +82,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif
+#endif /* HASH_TABLES_H */
